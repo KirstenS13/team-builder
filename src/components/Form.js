ac20c9b3cb856adbe0216f98642a3d6c22251edd
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-10 19:37:15 -0400

    in Form function returned form tag, label and input tags for name

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function Form(){
-
+    return (
+        <form>
+            <label htmlFor='name'>Name:</label>
+            <input 
+                type='text' 
+                id='name' 
+                name='name' 
+                placeholder='Type your full name here...'
+            />
+        </form>
+    );
 }
 
 export default Form;
