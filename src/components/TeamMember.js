85f4a0b913c0150139a54c775fd9d00637bd5e51
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-05-10 21:12:29 -0400

    gave each new teamMember a unique key attribute

--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -5,7 +5,7 @@ function TeamMember(props){
         <div>
             {props.team.map((teamMember) => {
                 return (
-                    <div>
+                    <div key={teamMember.id}>
                         <h4>Name: {teamMember.name}</h4>
                         <p>Email: {teamMember.email}</p>
                         <p>Role: {teamMember.role}</p>
